6cd08c37f7719ca03e0a1138b1d86f3350ed250f
Joshua PC <Joshua PC@JOSHUA-PC>
2024-06-01 22:29:56 +0800

    Add project files.

---
+++ WebApp_Salon/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp_Salon.Pages.Account
+{
+    public class LoginModel:PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+
+        public void OnGet() { 
+        }
+        public void OnPost()
+        {
+
+        }
+
+    }
+    public class Credential
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
